4e2799667932256bd13f3c2cf810376716b89079
anh212 <andrewha428@gmail.com>
2019-03-28 14:47:08 -0400

    can read thing
    added rest of can read

--- src/can.cpp
+++ src/can.cpp
@@ -8,6 +8,16 @@ void canInit(int pin1,pin2){
 }
 int canRead(int id, char[] name){
 	if(Can0.available(){
-	
+		CAN_message_t inMsg;
+    	Can0.read(inMsg);
+    
+    	if(inMsg.id == 0x5F0){
+      		volatile uint16_t rpm = (inMsg.buf[6] << 8) | inMsg.buf[7];
+      		// Serial.print(millis());
+      		// Serial.print(" ");
+      		Serial.println(rpm, DEC);
+      		Serial.println(rpm == 4935);
+      		//Serial.println(pw1);
+		}
 	}
 }
